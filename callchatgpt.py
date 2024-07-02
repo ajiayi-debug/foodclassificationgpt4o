9bb6b1635ee08f3af94a3c36326375bfc390ccc4
ajiayi-debug <e0774928@u.nus.edu>
2024-07-02 11:02:42 +0800

    trying to call gpt 4o

--- callchatgpt.py
+++ callchatgpt.py
@@ -2,6 +2,8 @@ import os
 import subprocess
 from openai import AzureOpenAI
 from environment import endpoint,tok
+import requests
+import base64
 from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 
 
@@ -15,14 +17,58 @@ client = AzureOpenAI(
   api_version="2024-02-01"
 )
 
+with open('./Onion.jfif')
+
 response = client.chat.completions.create(
-    model="gpt-35-turbo", # model = "deployment_name".
+    model="gpt-4o", # model = "deployment_name".
     messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
-        {"role": "assistant", "content": "Yes, customer managed keys are supported by Azure OpenAI."},
-        {"role": "user", "content": "Do other Azure AI services support this too?"}
-    ]
+        {"role": "system", "content": [
+            {"type": "text", 
+             "text": "You are a helpful assistant."}
+            ]},
+        {"role": "user", "content": [{
+            "type": "image_url",
+            "image_url":{
+                "url":image
+            }
+        },
+        {"type":"text",
+         "text":"Describe this image"}
+      ]
+    }
+  ]
 )
 
 print(response.choices[0].message.content)
+
+# Configuration
+# GPT4V_KEY = os.getenv("AZURE_OPENAI_API_KEY")
+# IMAGE_PATH = "C:/Users/angj02/OneDrive - FrieslandCampina/Desktop/test/Onion.jfif"
+# encoded_image = base64.b64encode(open(IMAGE_PATH, 'rb').read()).decode('ascii')
+# headers = {
+#     "Content-Type": "application/json",
+#     "api-key": GPT4V_KEY,
+# }
+
+# # Payload for the request
+# payload = {
+#   "messages": [
+#       {"role": "system", "content": "You are a helpful assistant."},
+#       {"role": "user", "content": "Describe the picture"}
+#   ],
+#   "temperature": 0.7,
+#   "top_p": 0.95,
+#   "max_tokens": 800
+# }
+
+# GPT4V_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
+
+# # Send request
+# try:
+#     response = requests.post(GPT4V_ENDPOINT, headers=headers, json=payload)
+#     response.raise_for_status()  # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
+# except requests.RequestException as e:
+#     raise SystemExit(f"Failed to make the request. Error: {e}")
+
+# # Handle the response as needed (e.g., print or process)
+# print(response.json())
